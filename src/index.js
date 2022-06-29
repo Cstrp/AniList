74fa6b4aa9b364b5d7a3302594421602f85a0b03
Cstrp <valery.nosareu@gmail.com>
2022-06-29 11:42:03 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import './styles/main.scss'
+import axios from 'axios'
+
+const search = document.querySelector('.search')
+const submit = document.querySelector('button')
+
+const searchURL = 'https://api.jikan.moe/v4/anime?q=&limit=5'
+const idURL = 'https://api.jikan.moe/v4/anime/{id}'
+// fetch(searchURL)
+//     .then((res)=> {
+//         return res.json()
+//     })
+//     .then((data) => {
+//         console.log(data)
+//     })
+// class Create
+console.log('Hello world!')
+
+fetch(searchURL)
+  .then(response => response.json())
+  .then(json => {
+    console.log(json)
+    const { data } = json
+    for (let i = 0; i < data.length; i++) {
+      createIMG(data[i].trailer.url)
+    }
+  })
+
+const container = document.querySelector('.container')
+
+function createIMG(source) {
+  const newIMG = document.createElement('img')
+  newIMG.src = source
+  container.append(newIMG)
+}
+
+axios
+  .get(searchURL)
+  .then(res => console.log(res))
+  .catch(e => console.log(e))
+
+// https://api.jikan.moe/v4/anime?q=${searchTerm}=&limit=30
+
+// https://api.jikan.moe/v4/anime
